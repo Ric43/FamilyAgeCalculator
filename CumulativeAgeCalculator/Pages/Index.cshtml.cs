46efa3e8d2bddd3bc3abf3e85437a5261b53cc97
Richard Chown <richie@email.local>
2020-09-03 13:05:00 +0200

    Initial Commit

---
+++ CumulativeAgeCalculator/Pages/Index.cshtml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using CumulativeAgeCalculator.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CumulativeAgeCalculator.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly AgeCalculatorService _ageCalculatorService;
+
+        public decimal AgeInDays { get; set; }
+        public decimal AgeInYears { get; set; }
+        public bool ShowAge { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [BindProperty]
+        public DateTime FirstPersonDob { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [BindProperty]
+        public DateTime Child1Dob { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [BindProperty]
+        public DateTime Child2Dob { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [BindProperty]
+        public DateTime Child3Dob { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [BindProperty]
+        public DateTime Child4Dob { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [BindProperty]
+        public DateTime Child5Dob { get; set; }
+
+        public IndexModel(AgeCalculatorService ageCalculatorService)
+        {
+            _ageCalculatorService = ageCalculatorService;
+            ShowAge = false;
+
+            if (FirstPersonDob == DateTime.MinValue)
+            {
+                FirstPersonDob = new DateTime(1946, 9, 14);
+                Child1Dob = new DateTime(1973, 9, 18);
+                Child2Dob = new DateTime(1975, 4, 7);
+                Child3Dob = new DateTime(1976, 12, 19);
+                Child4Dob = new DateTime(1983, 9, 2);
+                Child5Dob = new DateTime(1983, 9, 2);
+            }
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+            List<DateTime> otherDobs = new List<DateTime>();
+            otherDobs.Add(Child1Dob);
+            otherDobs.Add(Child2Dob);
+            otherDobs.Add(Child3Dob);
+            otherDobs.Add(Child4Dob);
+            otherDobs.Add(Child5Dob);
+
+            decimal resultInDays = _ageCalculatorService.CalculateAge(FirstPersonDob, otherDobs);
+            AgeInDays = resultInDays;
+            AgeInYears = resultInDays / 365;
+            ShowAge = true;
+        }
+    }
+}
---
+++ CumulativeAgeCalculator/Services/AgeCalculatorService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CumulativeAgeCalculator.Services
+{
+    public class AgeCalculatorService
+    {
+        private DateTime _firstPersonDob;
+        private List<DateTime> _otherDobs;
+
+        public AgeCalculatorService()
+        {
+        }
+
+        public decimal CalculateAge(DateTime firstPersonDob, List<DateTime> otherDobs)
+        {
+            _firstPersonDob = firstPersonDob;
+
+            if (otherDobs == null)
+            {
+                throw new ArgumentNullException(nameof(otherDobs));
+            }
+
+            if (otherDobs.Count == 0)
+            {
+                throw new ArgumentException(nameof(otherDobs));
+            }
+
+            otherDobs.Sort();
+            _otherDobs = otherDobs;
+
+            int cumulativeAgeOfOthersAtStart = _CalculateCumulateAgeOfOthersInDaysAtStart();
+            int ageOfFirstAtStart = _CalculateAgeOfFirstInDaysAtStart();
+
+            decimal age = (decimal)ageOfFirstAtStart + ((decimal)(ageOfFirstAtStart - cumulativeAgeOfOthersAtStart) / (decimal)(_otherDobs.Count - 1));
+            return age;
+        }
+
+        private int _CalculateCumulateAgeOfOthersInDaysAtStart()
+        {
+            DateTime youngest = _otherDobs.Last();
+            var diffs = 0;
+
+            if (_otherDobs.Count > 1)
+            {
+                IOrderedEnumerable<DateTime> others = _otherDobs.Take(_otherDobs.Count - 1).OrderByDescending(d => d);
+                foreach(DateTime other in others)
+                {
+                    diffs += (int)(youngest - other).TotalDays;
+                }
+            }
+
+            return diffs;
+        }
+
+        private int _CalculateAgeOfFirstInDaysAtStart()
+        {
+            DateTime youngest = _otherDobs.Last();
+            return (int)(youngest - _firstPersonDob).TotalDays;
+        }
+    }
+}
